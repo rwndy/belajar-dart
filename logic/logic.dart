dd24b07f7842f93cf93f7d7fbd8666fc9312ed25
rwndy <riwandy10@gmail.com>
2020-07-06 11:15:35 +0700

    belajar syntax logic

---
+++ logic/logic.dart
@@ -0,0 +1,32 @@
+main() {
+  // var umur = 15;
+
+  // if (umur > 17) {
+  //   print('kamu boleh membuat ektp');
+  // } else {
+  //   print('maaf, kamu belum bisa mendaftakan e-ktp');
+  // }
+
+  // ternary operator
+  // var ouput = umur > 17
+  //     ? 'kamu boleh membuat ektp'
+  //     : 'maaf, kamu belum bisa membuat e-ktp';
+  // print(ouput);
+
+  // menggunakan switch
+  // switch digunakan apabila nilainya tetap
+
+  // var pekerjaan = 'guru';
+  // var name = 'Aceng';
+
+  // switch (pekerjaan) {
+  //   case 'guru':
+  //     print('$name adalah seorang guru');
+  //     break;
+  //   case 'progammer':
+  //     print('$name adalah seorang programmer');
+  //     break;
+  //   default:
+  //     print('$name seorang pengangguran sejati');
+  // }
+}
