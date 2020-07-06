a6f2a51f966225e36ac937e70ef4ac86beea4924
rwndy <riwandy10@gmail.com>
2020-07-06 11:15:50 +0700

    belajar penjumlahan

--- tipe-data/part1.dart
+++ tipe-data/part1.dart
@@ -1,3 +1,13 @@
-void main() {
-  print('Hello World');
+// menggunakan void untuk me-return sesuatu di function
+// main() {
+//   var myName = 'Riwandi';
+//   var doingSomeThing = 'Sedang Makan';
+//   print('${myName} ${doingSomeThing}');
+// }
+
+// tipe data integer
+main() {
+  int angkaPertama = 10;
+  int angkaKedua = 130;
+  print(angkaPertama + angkaKedua);
 }
